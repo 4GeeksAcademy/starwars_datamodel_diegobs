efab58f8e4d52727dff6218443e3963d359ff06a
Diego Bermúdez <diegobermudezsantiago@hotmail.com>
2025-11-19 04:49:16 +0000

    Endpoints creados

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Characters, Planets, Species, Favorites
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,201 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+# generate sitemap with all your endpointsl
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# -------------------- Endpoints Favorites -------------------- #
+@app.route("/users/<int:user_id>/favorites", methods=["GET"])
+def getUserFavorites(user_id):
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    user = User.query.get(user_id)
+    result = [favorites.serialize() for favorites in user.favorites]
 
-    return jsonify(response_body), 200
+    return jsonify(result)
+
+
+
+@app.route("/users/<int:user_id>/favorites/<string:category>/<int:item_id>", methods=["POST"])
+def addFavorite(user_id, category, item_id):
+
+    validCategories = ["people", "planets"]
+    if category not in validCategories:
+        return jsonify({"error": "Categoría inválida"})
+    
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "Usuario no encontrado"})
+    
+    newFav = Favorites(category=category, item_id=item_id)
+
+    user.favorites.append(newFav)
+
+    db.session.add(newFav)
+    db.session.commit()
+    
+    return jsonify({"status": "ok","message": "¡Se ha añadido el favorito correctamente!"})
+
+
+
+@app.route("/users/<int:user_id>/favorites/<string:category>/<int:item_id>", methods=["DELETE"])
+def deleteFavorite(user_id, category, item_id):
+
+    validCategories = ["people", "planets"]
+    if category not in validCategories:
+        return jsonify({"error": "Categoría inválida"})
+    
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "Usuario no encontrado"})
+
+    result = Favorites.query.filter(Favorites.category == category, Favorites.item_id == item_id, Favorites.user.contains(user)).first()
+
+    db.session.delete(result)
+    db.session.commit()
+    
+    return jsonify({"status": "ok","message": "¡Se ha borrado el favorito correctamente!"})
+# ---------------------------------------------------------- #
+
+# -------------------- Endpoints User -------------------- #
+@app.route("/users", methods=["GET", "POST"])
+def getOrAddUser():
+
+    if (request.method == "GET"):
+        result = User.query.all()
+        result = [user.serialize() for user in result]
+
+        return jsonify(result)
+
+    elif (request.method == "POST"):
+        dataToPost = request.get_json()
+        result = User(
+            email=dataToPost["email"],
+            password=dataToPost["password"]
+        )
+        db.session.add(result)
+        db.session.commit()
+
+        return jsonify({"estado": "ok", "message": "¡El usuario se ha creado correctamente! :D"})
+# ---------------------------------------------------------- #
+
+# -------------------- Endpoints People (se llama Characters en la db) -------------------- #
+@app.route("/people", methods=["GET", "POST"])
+def getAll_or_addPeople():
+
+    if (request.method == "GET"):
+        result = Characters.query.all()
+        result = [characters.serialize() for characters in result]
+
+        return jsonify(result)
+
+    elif (request.method == "POST"):
+        dataToPost = request.get_json()
+        result = Characters(
+            name=dataToPost["name"],
+            gender=dataToPost["gender"],
+            species_id=dataToPost["species_id"],
+            weight=dataToPost["weight"],
+            height=dataToPost["height"],
+            hair_color=dataToPost["hair_color"],
+            eye_color=dataToPost["eye_color"],
+            birth_year=dataToPost["birth_year"],
+            homeworld_id=dataToPost["homeworld_id"]
+        )
+        db.session.add(result)
+        db.session.commit()
+
+        return jsonify({"estado": "ok", "message": "¡Se ha añadido el personaje correctamente!"})
+
+
+@app.route("/people/<int:id>", methods=["GET"])
+def getSingleCharacter(id):
+
+    result = Characters.query.filter_by(id=id).first()
+    return jsonify(result.serialize())
+
+
+@app.route("/people/<int:id>", methods=["DELETE"])
+def DeleteCharacter(id):
+
+    result = Characters.query.filter_by(id=id).first()
+
+    db.session.delete(result)
+    db.session.commit()
+
+    return jsonify({"estado": "ok", "message": "¡Se ha borrado el personaje correctamente!"})
+# ----------------------------------------------------------- #
+
+
+# -------------------- Endpoints Planets -------------------- #
+@app.route("/planets", methods=["GET", "POST"])
+def getAll_or_addPlanet():
+
+    if (request.method == "GET"):
+        result = Planets.query.all()
+        result = [planets.serialize() for planets in result]
+
+        return jsonify(result)
+
+    elif (request.method == "POST"):
+        dataToPost = request.get_json()
+        result = Planets(
+            name=dataToPost["name"],
+            climate=dataToPost["climate"],
+            terrain=dataToPost["terrain"],
+            population=dataToPost["population"],
+        )
+        db.session.add(result)
+        db.session.commit()
+
+        return jsonify({"estado": "ok", "message": "¡Se ha añadido el planeta correctamente!"})
+    
+@app.route("/planets/<int:id>", methods=["GET"])
+def getSinglePlanet(id):
+
+    result = Planets.query.filter_by(id=id).first()
+    return jsonify(result.serialize())
+# ----------------------------------------------------------- #
+
+
+# -------------------- Endpoints Species -------------------- #
+@app.route("/species", methods=["GET", "POST"])
+def getAll_or_addSpecies():
+
+    if (request.method == "GET"):
+        result = Species.query.all()
+        result = [species.serialize() for species in result]
+
+        return jsonify(result)
+
+    elif (request.method == "POST"):
+        dataToPost = request.get_json()
+        result = Species(
+            name=dataToPost["name"],
+            classification=dataToPost["classification"],
+            designation=dataToPost["designation"],
+            average_height=dataToPost["average_height"],
+            skin_colors=dataToPost["skin_colors"],
+            hair_colors=dataToPost["hair_colors"],
+            eye_colors=dataToPost["eye_colors"],
+            average_lifespan_in_years=dataToPost["average_lifespan_in_years"],
+            language=dataToPost["language"],
+            homeworld_id=dataToPost["homeworld_id"]
+        )
+        db.session.add(result)
+        db.session.commit()
+
+        return jsonify({"estado": "ok", "message": "¡Se ha añadido la especie correctamente!"})
+# ----------------------------------------------------------- #
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,9 +6,14 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 db = SQLAlchemy()
 
 # --- Bloque en el que se define las "opciones predefinidas" para los campos enum --- #
+
+
 class Gender(enum.Enum):
-    masculine = "masculine"         # No se puede usar boolean con otra cosa que no sean valores lógicos true o false 
-    femenine = "femenine"           # Para añadir una lista de "opciones predefinidas" es necesario usar Enum (enumeración)
+    # No se puede usar boolean con otra cosa que no sean valores lógicos true o false
+    masculine = "masculine"
+    # Para añadir una lista de "opciones predefinidas" es necesario usar Enum (enumeración)
+    femenine = "femenine"
+
 
 class Classification(enum.Enum):
     mammal = "mammal"
@@ -20,6 +25,7 @@ class Classification(enum.Enum):
     insectoid = "insectoid"
     unknown = "unknown"
 
+
 class Designation(enum.Enum):
     sentient = "sentient"
     non_sentient = "non-sentient"
@@ -28,12 +34,11 @@ class Designation(enum.Enum):
 # ----------------------------------------------------------------------------------- #
 
 
-# AÑADIR FAVORITOS DEL USUARIO #
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+    String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def serialize(self):
         return {
@@ -42,25 +47,52 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+        # --- Relaciones --- #
+    favorites: Mapped[list["Favorites"]] = relationship("Favorites", secondary="user_favorites", back_populates="user")
+    # ------------------ #
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    category = db.Column(db.String(20), nullable=False)
+    item_id = db.Column(db.Integer, nullable=False)
+
+    # --- Relaciones --- #
+    user: Mapped[list["User"]] = relationship("User", secondary="user_favorites", back_populates="favorites")
+    # ------------------ #
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "category": self.category,
+            "item_id": self.item_id
+        }
+
 class Characters(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(40))
     gender: Mapped[Gender] = mapped_column(Enum(Gender), nullable=False)
-    species_id: Mapped[int] = mapped_column(ForeignKey("species.id"))
+    species_id: Mapped[int] = mapped_column(
+        ForeignKey("species.id"), nullable=True)
     weight: Mapped[float] = mapped_column(Float)
+    height: Mapped[float] = mapped_column(Float)
     hair_color: Mapped[str] = mapped_column(String(20))
     eye_color: Mapped[str] = mapped_column(String(20))
     birth_year: Mapped[str] = mapped_column(String(10))
-    homeworld_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+    homeworld_id: Mapped[int] = mapped_column(
+        ForeignKey("planets.id"), nullable=True)
 
     # --- Relaciones --- #
-    homeworld: Mapped["Planets"] = relationship("Planets", back_populates="residents") 
-    species: Mapped["Species"] = relationship("Species", back_populates="residents")
-    films: Mapped[list["Films"]] = relationship(secondary="characters_films",back_populates="characters")
-    starships: Mapped[list["Starships"]] = relationship(secondary="characters_starships", back_populates="pilots")
-    vehicles: Mapped[list["Vehicles"]] = relationship(secondary="characters_vehicles", back_populates="pilots")
+    homeworld: Mapped["Planets"] = relationship(
+        "Planets", back_populates="residents")
+    species: Mapped["Species"] = relationship(
+        "Species", back_populates="residents")
+    films: Mapped[list["Films"]] = relationship(
+        secondary="characters_films", back_populates="characters")
+    starships: Mapped[list["Starships"]] = relationship(
+        secondary="characters_starships", back_populates="pilots")
+    vehicles: Mapped[list["Vehicles"]] = relationship(
+        secondary="characters_vehicles", back_populates="pilots")
     # ------------------ #
-
 
     def serialize(self):
         return {
@@ -74,7 +106,8 @@ class Characters(db.Model):
             "homeworld": self.homeworld.name if self.homeworld else None,
             "films": [film.title for film in self.films],
         }
-    
+
+
 class Films(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(40))
@@ -83,12 +116,15 @@ class Films(db.Model):
     producer: Mapped[str] = mapped_column(String(40))
     release_date: Mapped[str] = mapped_column(Date)
     opening_crawl: Mapped[str] = mapped_column(String(250))
-   
+
     # --- Relaciones --- #
-    characters: Mapped[list["Characters"]] = relationship(secondary="characters_films", back_populates="films")
-    starships: Mapped[list["Starships"]] = relationship(secondary="starships_films", back_populates="films")
-    vehicles: Mapped[list["Vehicles"]] = relationship(secondary="vehicles_films", back_populates="films")
-    # ------------------ #
+    characters: Mapped[list["Characters"]] = relationship(
+        secondary="characters_films", back_populates="films")
+    starships: Mapped[list["Starships"]] = relationship(
+        secondary="starships_films", back_populates="films")
+    vehicles: Mapped[list["Vehicles"]] = relationship(
+        secondary="vehicles_films", back_populates="films")
+   
 
     def serialize(self):
         return {
@@ -102,6 +138,7 @@ class Films(db.Model):
             "characters": [char.name for char in self.characters]
         }
 
+
 class Planets(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(40))
@@ -110,8 +147,10 @@ class Planets(db.Model):
     population: Mapped[int] = mapped_column(Integer)
 
     # --- Relaciones --- #
-    residents: Mapped[list["Characters"]] = relationship("Characters", back_populates="homeworld")
-    species: Mapped[list["Species"]] = relationship("Species", back_populates="homeworld")
+    residents: Mapped[list["Characters"]] = relationship(
+        "Characters", back_populates="homeworld")
+    species: Mapped[list["Species"]] = relationship(
+        "Species", back_populates="homeworld")
     # ------------------ #
 
     def serialize(self):
@@ -122,6 +161,7 @@ class Planets(db.Model):
             "terrain": self.terrain,
             "population": self.population
         }
+
 
 class Species(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -134,27 +174,30 @@ class Species(db.Model):
     eye_colors: Mapped[str] = mapped_column(String(100))
     average_lifespan_in_years: Mapped[str] = mapped_column(String(20))
     language: Mapped[str] = mapped_column(String(50))
-    homeworld_id: Mapped[int] = mapped_column(ForeignKey("planets.id")) # RELACIÓN 1 A 1 
+    homeworld_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=True)  # RELACIÓN 1 A 1
 
     # --- Relaciones --- #
-    residents: Mapped[list["Characters"]] = relationship("Characters", back_populates="species")
-    homeworld: Mapped["Planets"] = relationship("Planets", back_populates="species")
+    residents: Mapped[list["Characters"]] = relationship(
+        "Characters", back_populates="species")
+    homeworld: Mapped["Planets"] = relationship(
+        "Planets", back_populates="species")
     # ------------------ #
 
     def serialize(self):
         return {
-        "id": self.id,
-        "name": self.name,
-        "classification": self.classification,
-        "designation": self.designation,
-        "average_height": self.average_height,
-        "skin_colors": self.skin_colors,
-        "hair_colors": self.hair_colors,
-        "eye_colors": self.eye_colors,
-        "average_lifespan_in_years": self.average_lifespan_in_years,
-        "language": self.language,
-        "homeworld_id": self.homeworld_id,
+            "id": self.id,
+            "name": self.name,
+            "classification": self.classification.value,
+            "designation": self.designation.value,
+            "average_height": self.average_height,
+            "skin_colors": self.skin_colors,
+            "hair_colors": self.hair_colors,
+            "eye_colors": self.eye_colors,
+            "average_lifespan_in_years": self.average_lifespan_in_years,
+            "language": self.language,
+            "homeworld_id": self.homeworld_id,
         }
+
 
 class Starships(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -173,28 +216,31 @@ class Starships(db.Model):
     starship_class: Mapped[str] = mapped_column(String(20))
 
     # --- Relaciones --- #
-    pilots: Mapped[list["Characters"]] = relationship(secondary="characters_starships", back_populates="starships")
-    films: Mapped[list["Films"]] = relationship(secondary="starships_films", back_populates="starships")
+    pilots: Mapped[list["Characters"]] = relationship(
+        secondary="characters_starships", back_populates="starships")
+    films: Mapped[list["Films"]] = relationship(
+        secondary="starships_films", back_populates="starships")
     # ------------------ #
 
     def serialize(self):
-            return {
-                "id": self.id,
-                "name": self.name,
-                "model": self.model,
-                "manufacturer": self.manufacturer,
-                "cost_in_credits": self.cost_in_credits,
-                "length": self.length,
-                "max_atmosphering_speed": self.max_atmosphering_speed,
-                "crew": self.crew,
-                "passengers": self.passengers,
-                "cargo_capacity": self.cargo_capacity,
-                "consumables": self.consumables,
-                "hyperdrive_rating": self.hyperdrive_rating,
-                "MGLT": self.MGLT,
-                "starship_class": self.starship_class,
-                "pilots": [pil.name for pil in self.pilots]
-            }
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "hyperdrive_rating": self.hyperdrive_rating,
+            "MGLT": self.MGLT,
+            "starship_class": self.starship_class,
+            "pilots": [pil.name for pil in self.pilots]
+        }
+
 
 class Vehicles(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -210,8 +256,10 @@ class Vehicles(db.Model):
     vehicle_class: Mapped[str] = mapped_column(String(20))
 
     # --- Relaciones --- #
-    pilots: Mapped[list["Characters"]] = relationship(secondary="characters_vehicles",back_populates="vehicles")
-    films: Mapped[list["Films"]] = relationship(secondary="vehicles_films", back_populates="vehicles")
+    pilots: Mapped[list["Characters"]] = relationship(
+        secondary="characters_vehicles", back_populates="vehicles")
+    films: Mapped[list["Films"]] = relationship(
+        secondary="vehicles_films", back_populates="vehicles")
     # ------------------ #
 
     def serialize(self):
@@ -230,7 +278,6 @@ class Vehicles(db.Model):
             "pilots": [pil.name for pil in self.pilots]
         }
 
-    
 
 # ------------------- Tablas de intermedias ------------------- #
 class CharacterFilm(db.Model):
@@ -238,23 +285,32 @@ class CharacterFilm(db.Model):
     character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"), primary_key=True)
     film_id: Mapped[int] = mapped_column(ForeignKey("films.id"), primary_key=True)
 
+
 class CharacterStarship(db.Model):
     __tablename__ = "characters_starships"
     character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"), primary_key=True)
     starship_id: Mapped[int] = mapped_column(ForeignKey("starships.id"), primary_key=True)
+
 
 class CharacterVehicle(db.Model):
     __tablename__ = "characters_vehicles"
     character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"), primary_key=True)
     vehicle_id: Mapped[int] = mapped_column(ForeignKey("vehicles.id"), primary_key=True)
 
+
 class StarshipFilm(db.Model):
     __tablename__ = "starships_films"
     starship_id: Mapped[int] = mapped_column(ForeignKey("starships.id"), primary_key=True)
     film_id: Mapped[int] = mapped_column(ForeignKey("films.id"), primary_key=True)
 
+
 class VehicleFilm(db.Model):
     __tablename__ = "vehicles_films"
     vehicle_id: Mapped[int] = mapped_column(ForeignKey("vehicles.id"), primary_key=True)
     film_id: Mapped[int] = mapped_column(ForeignKey("films.id"), primary_key=True)
+
+class UserFavorites(db.Model):
+    __tablename__="user_favorites"
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+    favorite_id: Mapped[int] = mapped_column(ForeignKey("favorites.id"), primary_key=True)
 # ------------------------------------------------------------ #
